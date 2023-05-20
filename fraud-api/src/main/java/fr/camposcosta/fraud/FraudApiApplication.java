4b2e9285dca4dbbb5127f667d2d51388dabf1782
Arthur Campos Costa <artelio.camposta@gmail.com>
2023-05-20 12:06:03 +0900

    Add Fraud API module

---
+++ fraud-api/src/main/java/fr/camposcosta/fraud/FraudApiApplication.java
@@ -0,0 +1,12 @@
+package fr.camposcosta.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FraudApiApplication.class, args);
+    }
+}
