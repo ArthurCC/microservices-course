71707bb9b5262f2ffa745d11fae6ae91832820c3
Arthur Campos Costa <artelio.camposta@gmail.com>
2023-05-20 11:20:07 +0900

    Use sequence for customer id generation

--- customer-api/src/main/java/fr/camposcosta/customerapi/entity/Customer.java
+++ customer-api/src/main/java/fr/camposcosta/customerapi/entity/Customer.java
@@ -11,7 +11,15 @@ import javax.persistence.*;
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(
+            name = "customer_id_generator",
+            sequenceName = "customer_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_generator"
+    )
     private Integer id;
 
     @Column(nullable = false)
