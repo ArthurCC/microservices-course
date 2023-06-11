ecae6e4b8e41699b7b8f7891344099ad123d041d
Arthur Campos Costa <artelio.camposta@gmail.com>
2023-06-11 13:38:28 +0900

    Setup service communication with discovery and load balancing

--- customer-api/src/main/java/fr/camposcosta/customerapi/config/CustomerWebConfig.java
+++ customer-api/src/main/java/fr/camposcosta/customerapi/config/CustomerWebConfig.java
@@ -1,6 +1,7 @@
 package fr.camposcosta.customerapi.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,9 @@ import org.springframework.web.client.RestTemplate;
 public class CustomerWebConfig {
 
     @Bean
+    // This is necessary otherwise RestTemplate does not know it needs to communicate with discovery server
+    // to get target service location
+    @LoadBalanced
     RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder.build();
     }
