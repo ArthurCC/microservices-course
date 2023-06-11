d1a104f9e2828fb8fbcd75681d73e450e99e72e6
Arthur Campos Costa <artelio.camposta@gmail.com>
2023-06-11 13:01:13 +0900

    Setup service discovery

--- customer-api/src/main/java/fr/camposcosta/customerapi/CustomerApiApplication.java
+++ customer-api/src/main/java/fr/camposcosta/customerapi/CustomerApiApplication.java
@@ -2,8 +2,10 @@ package fr.camposcosta.customerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerApiApplication {
 
     public static void main(String[] args) {
--- fraud-api/src/main/java/fr/camposcosta/fraudapi/FraudApiApplication.java
+++ fraud-api/src/main/java/fr/camposcosta/fraudapi/FraudApiApplication.java
@@ -2,8 +2,10 @@ package fr.camposcosta.fraudapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FraudApiApplication {
 
     public static void main(String[] args) {
